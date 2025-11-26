a50d0348bac5995f25379190f40ef5abd353496c
jammees <86011536+jammees@users.noreply.github.com>
2025-11-26 18:30:55 +0100

    Add some todos with multiple situations

--- Code/MyComponent.cs
+++ Code/MyComponent.cs
@@ -3,6 +3,22 @@ public sealed class MyComponent : Component
 {
 	[Property] public string StringProperty { get; set; }
 
+	// TODO: finish working on this
+	public void Empty()
+	{
+		// TODO: this is inside of the thing
+		// TODO: under this one ^
+	}
+
+	//toDo:this is some uneven formatting
+	//with multilines
+	public void Empty2()
+	{
+	}
+
+	// TODO: this is a todo message
+	//
+	// that expans multiple lines
 	protected override void OnUpdate()
 	{
 		Log.Info( "wara2" );
