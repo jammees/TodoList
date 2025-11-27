40025c5f3c70b1a556aca74d8d99a40b2340fe70
jammees <86011536+jammees@users.noreply.github.com>
2025-11-27 20:25:10 +0100

    Compact Code and Todo entry into single file

---
+++ Libraries/TodoList/Editor/List/Entries.cs
@@ -0,0 +1,20 @@
+﻿namespace Todo.List;
+
+internal class Entry
+{
+	public string Message { get; set; } = "None";
+
+	public string Group { get; set; } = "Default";
+}
+
+internal sealed class CodeEntry: Entry
+{
+	public string SourceFile { get; set; } = "";
+
+	public int SourceLine { get; set; } = 0;
+}
+
+internal sealed class TodoEntry: Entry
+{
+	public bool IsDone { get; set; } = false;
+}
