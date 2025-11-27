ba9947bba1fd9d750c8961fe9f46c0b544879881
jammees <86011536+jammees@users.noreply.github.com>
2025-11-27 18:57:58 +0100

    Create CodeEntry.cs

---
+++ Libraries/TodoList/Editor/List/CodeEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.List;
+
+internal class CodeEntry
+{
+	public string Message { get; set; } = "";
+
+	public string SourceFile { get; set; } = "";
+
+	public int SourceLine { get; set; } = 0;
+}
