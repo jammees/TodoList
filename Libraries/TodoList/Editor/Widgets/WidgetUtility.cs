0229d6b4859bddd7c852bbd597c1002a75c38c22
jammees <86011536+jammees@users.noreply.github.com>
2025-12-11 21:10:34 +0100

    Don't use fixed size

--- Libraries/TodoList/Editor/Widgets/WidgetUtility.cs
+++ Libraries/TodoList/Editor/Widgets/WidgetUtility.cs
@@ -7,9 +7,10 @@ internal static class WidgetUtility
 	internal static void SetProperties( Widget widget, float height, string title, string icon )
 	{
 		widget.DeleteOnClose = true;
-		widget.FixedSize = new( 500f, height );
+		widget.MinimumSize = new( 500f, height );
 		widget.WindowTitle = title;
 		widget.SetWindowIcon( icon );
+		widget.FocusMode = FocusMode.Click;
 
 		if ( TodoDock.Cookies.WidgetsOnTop )
 		{
